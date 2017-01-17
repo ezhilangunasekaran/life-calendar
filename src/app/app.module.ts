983ac9c3556aaafe831b08f7020e2c0cf00e87c5
Ezhil <g.ezhilan1@gmail.com>
2017-01-17 13:23:49 -0600

    module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WeekComponent } from './week/week.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeekComponent
   ],
   imports: [
     BrowserModule,
