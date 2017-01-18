4a0bb442979e4aca7461536d6cd5cb95d35307e4
Ezhil <g.ezhilan1@gmail.com>
2017-01-17 22:59:58 -0600

    added crud function in week component

--- src/app/week/week.component.ts
+++ src/app/week/week.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Week} from "../week"; (Week)
+import {WeekService} from "../services/week.service"; (WeekService)
 
 @Component({
   selector: 'app-week',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./week.component.css']
 })
 export class WeekComponent implements OnInit {
-
-  constructor() { }
+  weeks = this._weekService.getWeeks();
+  weekarr : Object[] = (function(){
+    var input = [];
+    const MAX_AGE = 4693;
+    const ONE_DAY = 1000*60*60*24;
+    var dob = new Date(localStorage.getItem('lc-dob')).getTime();
+    var current_week = Math.round(((new Date().getTime()-dob)/ONE_DAY)/7);
+    for (let i = 1; i <= MAX_AGE; i++) {
+      if(i<current_week){
+        input.push([{no:i,class:"current"}]);
+      }else{
+        input.push([{no:i}]);
+      }
+    }
+    return input;
+  }());
+  constructor(private _weekService: WeekService) { }
 
   ngOnInit() {
   }
 
+  addWeekEvent(newweek){
+    var singleWeek: Week;
+    const ONE_DAY = 1000*60*60*24;
+    // from initial prompt
+    var dob = new Date(localStorage.getItem('lc-dob')).getTime();
+    // from date picker
+
+    var menWeek = new Date(newweek.nweek).getTime();
+    singleWeek.weekId = Math.round(((menWeek-dob)/ONE_DAY)/7);
+    singleWeek.weekTitle = newweek.title;
+    this._weekService.addWeekEvent(singleWeek);
+  }
+
+  deleteWeekEvent(index,mainindex){
+    var deleteWeek: Week;
+    this.weeks[mainindex].splice(index,1);
+    if(this.weeks[mainindex].length ==0){
+      delete this.weeks[mainindex];
+    }
+    deleteWeek.weekId = mainindex;
+    deleteWeek.weekTitle = index.replace(/<(?:.|\n)*?>/gm, '');
+    this._weekService.deleteWeekEvent(deleteWeek);
+  }
 }
