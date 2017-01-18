b7a12b35e95d16ab253d8a7c1843c9d94d374830
Ezhil <g.ezhilan1@gmail.com>
2017-01-18 12:57:12 -0600

    added week service to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { WeekComponent } from './week/week.component';
+import { WeekService } from './services/week.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { WeekComponent } from './week/week.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ WeekService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
