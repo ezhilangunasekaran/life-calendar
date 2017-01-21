838d8907817f4da31e74bf2216da8b994b9208d7
Ezhil <g.ezhilan1@gmail.com>
2017-01-21 17:54:58 -0600

    added capture DOB component in module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   template: '<app-week></app-week>',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { DatePickerModule } from 'ng2-datepicker';
 
 import { AppComponent } from './app.component';
 import { WeekComponent } from './week/week.component';
 import { WeekService } from './services/week.service';
+import { CapturedobComponent } from './capturedob/capturedob.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeekComponent
+    WeekComponent,
+    CapturedobComponent
   ],
   entryComponents: [
-    AppComponent,WeekComponent
+    AppComponent,WeekComponent,CapturedobComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    DatePickerModule
   ],
   providers: [ WeekService ],
   bootstrap: [AppComponent]
