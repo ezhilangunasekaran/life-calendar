bc77b5dcceb559b2ef7750128076f6e8290f27b8
Ezhil <g.ezhilan1@gmail.com>
2017-01-21 17:57:33 -0600

    added functionality to get and set DOB in week service

--- src/app/services/week.service.ts
+++ src/app/services/week.service.ts
@@ -10,7 +10,39 @@ export class WeekService extends Init{
     console.log("Week service started");
     this.load();
   }
+  dob: string = localStorage.getItem('lc-dob');
+  // set DOB
+  setDob(birthday:string){
+    this.dob = birthday;
+    localStorage.setItem('lc-dob',birthday);
+    return this;
+  }
+  getDob(){
+    return this.dob;
+  }
+  getCurrentWeeks(){
+    let input = [];
+    const MAX_AGE = 4693;
+    const ONE_DAY = 1000*60*60*24;
+    let current_week;
+    if(this.getDob()){
+      console.log("init");
+      let dob = new Date(this.getDob()).getTime();
+      current_week = Math.round(((new Date().getTime()-dob)/ONE_DAY)/7);
+    }else {
+      current_week = 0
+    }
 
+    for (let i = 1; i <= MAX_AGE; i++) {
+      if(i<current_week){
+        input.push([{no:i,class:"current"}]);
+      }else{
+        input.push([{no:i}]);
+      }
+    }
+    //console.log(input);
+    return input;
+  }
   weeks: {} = JSON.parse(localStorage.getItem('life-calendar'));
 
   getWeeks(){
