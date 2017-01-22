9636981a49c71d783dd57f1e865c3f220d54babb
Ezhil <g.ezhilan1@gmail.com>
2017-01-22 16:47:48 -0600

    added function addweekDialog

--- src/app/week/week.component.ts
+++ src/app/week/week.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Week} from "../week"; (Week)
 import {WeekService} from "../services/week.service"; (WeekService)
 import { CapturedobComponent } from '../capturedob/capturedob.component'
+import { AddweekdialogComponent } from '../addweekdialog/addweekdialog.component';
 import { MdDialog } from '@angular/material';
 
 @Component({
@@ -10,17 +11,35 @@ import { MdDialog } from '@angular/material';
   styleUrls: ['./week.component.css']
 })
 export class WeekComponent implements OnInit {
+  // 90yrs week list generated from weekservice
   weekarr : Object[] = this._weekService.getCurrentWeeks();
+
+  // list of weeks and their corresponding events from localStorage
   weeks = JSON.parse(localStorage.getItem('life-calendar'));
+
   enteredDob: string;
+  lastDialogResult: Object;
+  singleWeek: Week = {weekId:0,weekTitle:null};
+  deleteWeek: Week;
+
   constructor(private _weekService: WeekService,public _dialog: MdDialog) { }
 
+  // DOB dialog popup
   dobDialog(){
     let dialogRef = this._dialog.open(CapturedobComponent);
     dialogRef.afterClosed().subscribe(result =>{
       this.enteredDob = result;
       this._weekService.setDob(result);
       this.weekarr = this._weekService.getCurrentWeeks();
+    })
+  }
+
+  // add week dialog
+  addweekDialog(){
+    let dialogRef = this._dialog.open(AddweekdialogComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      this.lastDialogResult = result;
+      this.addWeekEvent(this.lastDialogResult);
     })
   }
 
@@ -38,26 +57,34 @@ export class WeekComponent implements OnInit {
 
   //CRUD operation
   addWeekEvent(newweek){
-    var singleWeek: Week;
     const ONE_DAY = 1000*60*60*24;
-    // from initial prompt
-    var dob = new Date(localStorage.getItem('lc-dob')).getTime();
-    // from date picker
+    var menWeek;
+    var dob;
 
-    var menWeek = new Date(newweek.nweek).getTime();
-    singleWeek.weekId = Math.round(((menWeek-dob)/ONE_DAY)/7);
-    singleWeek.weekTitle = newweek.title;
-    this._weekService.addWeekEvent(singleWeek);
+    if(this._weekService.getDob() == null){
+      dob = new Date().getTime();
+    }else {
+     dob = new Date(this._weekService.getDob()).getTime();
+    }
+    menWeek = new Date(newweek.nweek).getTime();
+
+    this.singleWeek.weekId = Math.round(((menWeek-dob)/ONE_DAY)/7);
+    this.singleWeek.weekTitle = newweek.title;
+    if(!this.weeks[this.singleWeek.weekId]){
+      this.weeks[this.singleWeek.weekId] = [];
+    }
+    this.weeks[this.singleWeek.weekId].push(this.singleWeek.weekTitle);
+    this._weekService.addWeekEvent(this.singleWeek);
+
   }
 
   deleteWeekEvent(index,mainindex){
-    var deleteWeek: Week;
     this.weeks[mainindex].splice(index,1);
     if(this.weeks[mainindex].length ==0){
       delete this.weeks[mainindex];
     }
-    deleteWeek.weekId = mainindex;
-    deleteWeek.weekTitle = index.replace(/<(?:.|\n)*?>/gm, '');
-    this._weekService.deleteWeekEvent(deleteWeek);
+    this.deleteWeek.weekId = mainindex;
+    this.deleteWeek.weekTitle = index.replace(/<(?:.|\n)*?>/gm, '');
+    this._weekService.deleteWeekEvent(this.deleteWeek);
   }
 }
